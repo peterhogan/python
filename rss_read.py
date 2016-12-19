992c25f493bf02ea82b9dd669e86bad7a467be2f
Peter Hogan <peterjameshogan@gmail.com>
2016-12-19 23:10:41 +0000

    added rss reader

---
+++ rss_read.py
@@ -0,0 +1,26 @@
+from lxml import etree
+import urllib.request
+import shutil
+
+#url = "http://feeds.bbci.co.uk/news/rss.xml?edition=int"
+url = "http://feeds.bbci.co.uk/news/video_and_audio/technology/rss.xml"
+
+with urllib.request.urlopen(url) as response, open('bbc_rss1.xml', 'wb') as out_file:
+    shutil.copyfileobj(response, out_file)
+
+response = urllib.request.urlopen(url)
+data = response.read()
+text = data.decode('utf-8')
+
+bbc_rss = etree.parse('bbc_rss1.xml')
+#bbc_rss = etree.parse(out_file)
+
+bbc_titles = bbc_rss.xpath('//channel/item/title')
+bbc_desc = bbc_rss.xpath('//channel/item/description')
+bbc_date = bbc_rss.xpath('//channel/item/pubDate')
+
+for i in range(len(bbc_titles)):
+    print("#############")
+    print(bbc_titles[i].text)
+    print(bbc_desc[i].text)
+    print(bbc_date[i].text)
