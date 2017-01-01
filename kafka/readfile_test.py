0aced6accf1b5f5a625b2f70fcdc66a236381c15
Peter Hogan <peterjameshogan@gmail.com>
2017-01-01 16:52:00 +0000

    tidy ups

--- kafka/readfile_test.py
+++
@@ -1,7 +0,0 @@
-with open('rssfeeds.txt') as rssfile:
-    lines = rssfile.read().splitlines()
-for line in lines:
-    if line.startswith('http'):
-        print(line)
-    else: 
-        pass
--- kafka/rundocker_container.sh
+++
@@ -1,1 +0,0 @@
-sudo docker run -v /home/pine/git/python/kafka:/data -P -it peterjameshogan/storm-kafka-python:1 bash
