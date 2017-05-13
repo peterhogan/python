ab10592d41e2ccadff85e811d04f3f3c90b0a7de
Peter Hogan <peterjameshogan@gmail.com>
2017-05-13 08:51:28 +0100

    changes

--- rss_read.py
+++ rss_read.py
@@ -2,8 +2,8 @@ from lxml import etree
 import urllib.request
 import shutil
 
-#url = "http://feeds.bbci.co.uk/news/rss.xml?edition=int"
-url = "http://feeds.bbci.co.uk/news/video_and_audio/technology/rss.xml"
+url = "http://feeds.bbci.co.uk/news/rss.xml?edition=int"
+#url = "http://feeds.bbci.co.uk/news/video_and_audio/technology/rss.xml"
 
 with urllib.request.urlopen(url) as response, open('bbc_rss1.xml', 'wb') as out_file:
     shutil.copyfileobj(response, out_file)
@@ -21,6 +21,4 @@ bbc_date = bbc_rss.xpath('//channel/item/pubDate')
 
 for i in range(len(bbc_titles)):
     print("#############")
-    print(bbc_titles[i].text)
-    print(bbc_desc[i].text)
-    print(bbc_date[i].text)
+    print(bbc_titles[i].text,"|",bbc_desc[i].text,"|",bbc_date[i].text)
